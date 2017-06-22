2bec0b4f4d9fd0baa57299da91cb6527313187c4
Dknife <young.min.kang@gmail.com>
2017-06-22 17:11:12 +0900

    histogram2

--- matplotlib/02Histogram.py
+++ matplotlib/02Histogram.py
@@ -2,24 +2,19 @@ import matplotlib.pyplot as plt
 
 import numpy as np
 import matplotlib.pyplot as plt
-import matplotlib
-import matplotlib.font_manager as fm
 
-print(matplotlib.rcParams["font.family"])
+N = 100000
+x = np.linspace(0, 100, N)
+y = 100 + 15*np.random.randn(N)
+colors = y
 
-
-font_location = "c:/windows/fonts/HYKANB.ttf"
-font_name = fm.FontProperties(fname=font_location).get_name()
-matplotlib.rc('font', family=font_name)
-
-x = np.linspace(0, 100, 1000)
-y = 100 + 15*np.random.randn(1000)
 
 plt.figure(1)
 ax1 = plt.subplot(211) # 2 rows, 1 column, figure 1
-plt.scatter(x, y, alpha=0.2);
+
+plt.scatter(x, y, c=colors, cmap='cool', alpha=0.2)
 ax2 = plt.subplot(212) # 2 rows, 1 column, figure 2
-n, bins, patches = plt.hist(y, 50, normed=1, color='g', alpha=0.5)
+n, bins, patches = plt.hist(y, 100, normed=1, color='b',  alpha=0.75)
 plt.grid(True)
 print(n, bins, patches)
 
