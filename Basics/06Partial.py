6906e2ba9e2ec9d472c2188d78e47b4c14c2fd3d
Dknife <young.min.kang@gmail.com>
2017-08-28 14:53:39 +0900

    springdamper

--- Basics/06Partial.py
+++ Basics/06Partial.py
@@ -5,5 +5,12 @@ from functools import partial
 def mult(x,y) :
     return x*y
 
+def power(x,y) :
+    return y**x
+
 dbl = partial(mult, 2)
+sqr = partial(power, 2)
+
 print(dbl(5))
+print(sqr(5))
+
